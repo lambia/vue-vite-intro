db6cf0536d21a9365d093894573f9dee2a52092b
Luca Lambia <luca@lambia.it>
2026-03-03 12:01:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+	name: "App",
+	data() {
+		return {
+			// numero: 42
+		}
+	},
+	components: {
+		// HelloWorld
+	},
+	methods: {},
+	mounted() {
+		console.log("Componente app montato");
+	}
+}
+</script>
+
+<template>
+	<h1>Benvenuto in Vue+Vite</h1>
+	<!-- <p>{{ numero }}</p> -->
+	<!-- <HelloWorld msg="Messaggio dal padre" /> -->
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+	name: "HelloWorld",
+	data() {
+		return {
+			count: 0
+		}
+	},
+	methods: {
+		faiCose() {
+			console.log("Faccio cose vedo gente");
+		},
+		incrementaContatore() {
+			this.count++;
+		}
+	},
+	mounted() {
+		console.log("Componente helloworld montato");
+	}
+}
+</script>
+
+<template>
+	<button @click="incrementaContatore">You clicked me {{ count }} times.</button>
+</template>
+
+<style scoped></style>
