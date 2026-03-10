4135d75d07528a3dc3c64457dda426fcd2750a82
Luca Lambia <luca@lambia.it>
2026-03-10 11:01:29 +0100

    one way binding  con axios

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
+
+// const axios = require('axios').default; //CJS
+import axios from "axios"; //MJS/ESM
 
 export default {
 	name: "App",
 	data() {
 		return {
-			// numero: 42
+			apiUrl: "https://jsonplaceholder.typicode.com/todos",
+			todoList: []
 		}
 	},
 	components: {
 		// HelloWorld
+		TodoList
 	},
-	methods: {},
+	methods: {
+	},
 	mounted() {
-		console.log("Componente app montato");
+		// console.log("Componente app montato");
+
+		axios.get(this.apiUrl).then(result => {
+			// console.log("Dati ricevuti!");
+			this.todoList = result.data;
+		});
 	}
 }
 </script>
 
 <template>
 	<h1>Benvenuto in Vue+Vite</h1>
+
 	<!-- <p>{{ numero }}</p> -->
 	<!-- <HelloWorld msg="Messaggio dal padre" /> -->
+	<TodoList :list="todoList" />
 </template>
 
 <style scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+	name: "TodoList",
+	props: ["list"],
+	data() {
+		return {
+			todoList: []
+		}
+	},
+	methods: {
+	},
+	computed: {
+		computedList() {
+			return this.list //.filter(todo => todo.id > 190)
+		}
+	},
+	mounted() {
+		// console.log(this.list);
+		// console.log("Componente mounted", this.list.length);
+		// this.todoList = this.list;
+	},
+	updated() {
+		// console.log("Componente updated", this.list.length);
+		// this.todoList = this.list;
+	}
+}
+</script>
+
+<template>
+	<h2>ToDo List:</h2>
+	<ul>
+		<li v-for="elemento in computedList">{{ elemento.title }}</li>
+	</ul>
+</template>
+
+<style scoped></style>
